159bbe090313ae50f357fbc7965a5ca6dce0aecd
DapperCloud <quentin.bonnet.work@gmail.com>
2018-04-29 14:29:16 +0200

    twitter: follow/unfollow d'utilisateurs, et listing de ceux-ci. Pas d'autre incidence pour l'instant

--- twitter/twitter.js
+++ twitter/twitter.js
@@ -9,6 +9,12 @@ var contents = fs.readFileSync("mdp.json");
 var passJson = JSON.parse(contents);
 console.log("OK !");
 
+console.log("Parsing des comptes suivis...");
+var contents = fs.readFileSync("followed.json");
+var followedUsers = JSON.parse(contents);
+console.log("OK !");
+
+
 function writeObj(obj, message) {
   if (!message) { message = obj; }
   var details = "*****************" + "\n" + message + "\n";
@@ -53,6 +59,46 @@ function linkTweet(data, channel) {
 	channel.send("https://twitter.com/"+data.user.screen_name+"/status/"+data.id_str);
 }
 
+function follow(screen_name, channel) {
+	if(screen_name in followedUsers) {
+		channel.send(screen_name+" est déjà suivi !");
+		return;
+	}
+	// On va chercher le dernier tweet pour le stocker (et vérifier au passage que l'utilisateur existe)
+	twitter.getUserTimeline({ screen_name: screen_name, count: '1'}, function(err, reponse, body) {
+		logTwitterError(err);
+		channel.send("Une erreur est survenue. Êtes-vous sûr que ce nom d'utilisateur existe..?");
+	}, function(data) {
+		var tweet = JSON.parse(data)[0];
+		followedUsers[screen_name] = tweet.id_str;
+		console.log(followedUsers);
+		fs.writeFileSync("followed.json", JSON.stringify(followedUsers));
+		channel.send("L'utilisateur "+screen_name+" est maintenant suivi !");
+	});
+}
+
+function unfollow(screen_name, channel) {
+	if(!(screen_name in followedUsers)) {
+		channel.send(screen_name+" n'est pas suivi !");
+		return;
+	}
+	delete followedUsers[screen_name];
+	fs.writeFileSync("followed.json", JSON.stringify(followedUsers));
+	channel.send(screen_name+" n'est maintenant plus suivi !");
+}
+
+function listFollowed(channel) {
+	if(Object.keys(followedUsers).length == 0) {
+		channel.send("Aucun utilisateur n'est suivi !");
+		return;
+	}
+	var message = "Voici la liste des utilisateurs actuellement suivis :"
+	for(user in followedUsers) {
+		message += "\n\t- **"+user+"**";
+	}
+	channel.send(message);
+}
+
 // Connection
 client.login(passJson.pass);
 client.on('ready', () => {
@@ -81,12 +127,28 @@ client.on('message', message => {
 			linkTweet(JSON.parse(data)[0], twitterChan);
 		});
 	}
+
+	if (args[0] === "t/follow") {
+		if(args.length <= 1 || args[1].length <= 1) return;
+		follow(args[1], twitterChan);
+	}
+
+	if (args[0] === "t/unfollow") {
+		if(args.length <= 1 || args[1].length <= 1) return;
+		unfollow(args[1], twitterChan);
+	}
+
+	if (args[0] === "t/list") {
+		listFollowed(twitterChan);
+	}
 	
 	if (args[0] === "t/help") {
 		message.reply("Cui cui, je suis là pour vous permettre d'accéder au caca de Twitter directement depuis Discord !\n"
 		+"Mes commandes :\n"
 		+"**********************************************\n"
-		+"t/last [nom_twitter] - affiche le dernier tweet de l'utilisateur\n");
+		+"t/last [nom_twitter] - affiche le dernier tweet de l'utilisateur\n"
+		+"t/follow [nom_twitter] - suivre un utilisateur\n"
+		+"t/unfollow [nom_twitter] - ne plus suivre un utilisateur");
 	}
 });
 
