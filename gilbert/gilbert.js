5eea1ca34b821b563497f13f336dc99e0fb7eae7
DapperCloud <quentin.bonnet@engagetech.com>
2018-05-10 13:51:38 +0200

    gilbert/lepers: tri des scores dans l'ordre décroissant

--- gilbert/gilbert.js
+++ gilbert/gilbert.js
@@ -77,7 +77,7 @@ function getOrderedScores(scoresObj) {
 	    a = a[1];
 	    b = b[1];
 
-	    return a < b ? -1 : (a > b ? 1 : 0);
+	    return a > b ? -1 : (a < b ? 1 : 0);
 	});
 
 	return tuples;
