3db70ca7f6e14d971d071f9874659bf693788359
DapperCloud <quentin.bonnet.work@gmail.com>
2018-05-10 18:52:12 +0200

    coco: fix ignored channels during parsing

--- coco/coco.js
+++ coco/coco.js
@@ -80,7 +80,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-	if(message.author.bot || ["lepers, gilbert"].includes(message.channel.name)) {
+	if(message.author.bot || ["lepers", "gilbert"].includes(message.channel.name)) {
 		return;
 	}
 
